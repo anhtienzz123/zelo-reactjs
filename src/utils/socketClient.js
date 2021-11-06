d26d3bcdcd47be67c722805e3f1e8329751b75b1
anhtienzz123 <52915164+anhtienzz123@users.noreply.github.com>
2021-11-07 00:37:23 +0700

    Update CI/CD

--- src/utils/socketClient.js
+++ src/utils/socketClient.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 export let socket;
 
 export function init() {
-    socket = io(process.env.REACT_APP_SOCKET_URL, {
+    socket = io(process.env.REACT_APP_URL, {
         transports: ['websocket'],
     });
 }
